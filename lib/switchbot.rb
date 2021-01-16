98ebbceb3ba0e3cba703c8393aa5b3dea6eceeb5
Yoshiki Takagi <yoshiki.tkg@gmail.com>
2021-01-16 23:30:31 +0900

    first commit

---
+++ lib/switchbot.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "switchbot/version"
+
+module Switchbot
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ spec/switchbot_spec.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+RSpec.describe Switchbot do
+  it "has a version number" do
+    expect(Switchbot::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
